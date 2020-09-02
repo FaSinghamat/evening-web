b0e74ccf0244418c43311dbbf2b7c8c3547fd27b
MonsitPasida <53083364+MonsitPasida@users.noreply.github.com>
2020-09-02 19:17:25 +0700

    Updates

---
+++ precache-manifest.b9e496d6f603b7d16db0f992248e82aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaac62e6b010c845c442472990b01dcf",
+    "url": "/evening-web/index.html"
+  },
+  {
+    "revision": "c70efe5ea2e65385e97e",
+    "url": "/evening-web/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9aea20cc5b85f385d504",
+    "url": "/evening-web/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c70efe5ea2e65385e97e",
+    "url": "/evening-web/static/js/2.2c9fb03c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/evening-web/static/js/2.2c9fb03c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aea20cc5b85f385d504",
+    "url": "/evening-web/static/js/main.8237469e.chunk.js"
+  },
+  {
+    "revision": "84e0c26b9b2c3ce217c4",
+    "url": "/evening-web/static/js/runtime-main.15b63780.js"
+  }
+]);
